ccc4c0931b28c249c9f4b7d49470a7df7826e8ee
James Colton <james.c@pabau.com>
2021-03-15 16:46:15 +0000

    cleaner main.ts

--- apps/bridge-api/src/main.ts
+++ apps/bridge-api/src/main.ts
@@ -1,38 +1,51 @@
 import { ApolloServer } from 'apollo-server-express'
-import express from 'express';
+import express from 'express'
 import { schema } from './schema'
-import { createContext } from './context'
 import cookieSession from 'cookie-session'
-import authenticatedUser from './middlewares/authenticatedUser'
 import cors from 'cors'
+import { stringToBoolean } from './utils'
+import { version } from '../../../package.json'
+import { createContext } from './context'
+import authenticatedUser from './middlewares/authenticatedUser'
 
-const PORT = 4000;
-const app = express();
-app.set('trust proxy', true);
-app.use(cors())
-  .use(express.urlencoded({
-    extended: true
-  }))
-  .use(cookieSession({
+console.log(`Starting bridge-api version ${version}`)
+console.log(
+  `Secure cookies: ` +
+    (stringToBoolean(process.env['SECURE_COOKIES']) ? 'ON' : 'OFF')
+)
+
+const PORT = process.env['PORT'] || 4000
+
+const app = express()
+app.set('trust proxy', true)
+app
+  .use(cors())
+  .use(
+    express.urlencoded({
+      extended: true,
+    })
+  )
+  .use(
+    cookieSession({
       signed: false,
-      secure: false,
-    }))
+      secure: stringToBoolean(process.env['SECURE_COOKIES']),
+      httpOnly: true,
+    })
+  )
   .use(authenticatedUser)
-
 
 const server = new ApolloServer({
   schema,
   context: createContext,
   tracing: true,
-});
+  playground: {
+    settings: {
+      'schema.polling.enable': false,
+      'request.credentials': 'include',
+    },
+  },
+})
 
-server.applyMiddleware({app})
-app.listen(
-  { port: PORT },
-  () =>
-    console.log(
-      `Server running on port ${PORT}`,
-    ),
-)
+server.applyMiddleware({ app })
 
-
+app.listen({ port: PORT }, () => console.log(`Server running on port ${PORT}`))
