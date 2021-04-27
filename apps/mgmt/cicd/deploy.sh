e805b071b6dab8fddfc3fd8e27ba1f13131cea7a
James Colton <james.c@pabau.com>
2021-04-27 18:24:09 +0100

    CI: limit slack to 3k chrs so it always posts, speed up master pipeline, nerf mgmt deploy

--- apps/mgmt/cicd/deploy.sh
+++ apps/mgmt/cicd/deploy.sh
@@ -1,12 +1,49 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+#
+# Deploys to vercel pro
+#
+
+# Stop on error
 set -e
 
-echo "DEBUG: pwd=$(pwd)"
-APP_NAME="$(basename "$(dirname "$(cd "$(dirname "${0}")"; pwd)")")"
-echo "DEBUG: app_name=${APP_NAME}"
+NEWLINE="
+"
 
-yarn run nx run "${APP_NAME}:build" --prod
-cp "apps/${APP_NAME}/vercel.json" "dist/apps/${APP_NAME}/"
-cp package.json "dist/apps/${APP_NAME}/"
-cp yarn.lock "dist/apps/${APP_NAME}/"
+read_heredoc() {
+  read_heredoc_result=""
+  while IFS="${NEWLINE}" read -r read_heredoc_line; do
+    read_heredoc_result="${read_heredoc_result}${read_heredoc_line}${NEWLINE}"
+  done
+  eval $1'=${read_heredoc_result}'
+}
+
+APP_NAME="$(basename "$(dirname "$(
+  cd "$(dirname "${0}")"
+  pwd
+)")")"
+APP_TYPE="$(basename "$(dirname "$(
+  cd "$(dirname "${0}")/.."
+  pwd
+)")")"
+VERCEL_JSON_LOCATION=$(cd "${APP_TYPE}/${APP_NAME}" && pwd)
+
+echo "----- DEBUG -----"
+echo "(pwd)=$(pwd)"
+echo "DOCKER_HOSTNAME=${DOCKER_HOSTNAME}"
+echo "DOCKER_USERNAME=${DOCKER_USERNAME}"
+echo "NODE_OPTIONS=${NODE_OPTIONS}"
+echo "APP_NAME=${APP_NAME}"
+echo "APP_TYPE=${APP_TYPE}"
+echo "VERCEL_JSON_LOCATION=${VERCEL_JSON_LOCATION}"
+echo "BITBUCKET_COMMIT=${BITBUCKET_COMMIT}"
+echo "BITBUCKET_PR_ID=${BITBUCKET_PR_ID}"
+echo "-----------------"
+
+#TODO
+
+#yarn run nx run "${APP_NAME}:build" --prod
+#cp "apps/${APP_NAME}/vercel.json" "dist/apps/${APP_NAME}/"
+#cp package.json "dist/apps/${APP_NAME}/"
+#cp yarn.lock "dist/apps/${APP_NAME}/"
 #cd "dist/apps/${APP_NAME}"
