17b4e78a0e1c603168badc4167965cf5387ea6ce
James Colton <james.c@pabau.com>
2021-05-07 19:23:59 +0100

    CI: lint-staged
    CI: lint-staged tsc
    
    CI: lint-staged eslint
    
    CI: lint-staged prettier ignore eslint files
    
    CI: pre-push now does tsc+workspace+ls instead of nx lint
    
    CI: fixed tsConfig->tsconfig test warning

---
+++ lint-staged.config.js
@@ -0,0 +1,30 @@
+// lint-staged.config.js
+const micromatch = require('micromatch')
+const prettier = require('prettier')
+
+const prettierSupportedExtensions = prettier
+  .getSupportInfo()
+  .languages.map(({ extensions }) => extensions)
+  .flat()
+  .filter((v) => ![".jsx",".js",".tsx",".ts"].includes(v))
+
+const addQuotes = (a) => `"${a}"`
+
+module.exports = (allStagedFiles) => {
+  const prettierFiles = micromatch(
+    allStagedFiles,
+    prettierSupportedExtensions.map((extension) => `**/*${extension}`)
+  )
+  const ret = []
+  if (prettierFiles.length > 0)
+    ret.push(`prettier --write ${prettierFiles.map(addQuotes).join(' ')}`)
+
+  // const tscFiles = micromatch(allStagedFiles, ['**/*.{tsx,ts}'])
+  // if (tscFiles.length > 0) ret.push(`tsc --noEmit ${tscFiles.map(addQuotes).join(' ')}`)
+
+  const eslintFiles = micromatch(allStagedFiles, ['**/*.{tsx,ts,jsx,js}'])
+  if (eslintFiles.length > 0)
+    ret.push(`eslint --fix ${eslintFiles.map(addQuotes).join(' ')}`)
+
+  return ret
+}
