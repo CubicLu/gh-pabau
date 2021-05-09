c388eba9eb2c30266c2a6ff32aa06eabd9eda582
James Colton <james.c@pabau.com>
2021-05-09 10:56:13 +0100

    Testing master pipeline by removing root page grid demo

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,24 +1,20 @@
-import { Button } from '@pabau/ui'
-import React, { FC, useContext, useState } from 'react'
+import React, { FC, useContext } from 'react'
 import { version } from '../../../package.json'
 import CommonHeader from '../components/CommonHeader'
-import Grid from '../components/Grid'
 import Layout from '../components/Layout/Layout'
 import { UserContext } from '../context/UserContext'
 import { useTranslationI18 } from '../hooks/useTranslationI18'
 import Login from './login'
+
 const Index: FC = () => {
   const { t } = useTranslationI18()
-  const [showGrid, setShowGrid] = useState(false)
   const user = useContext(UserContext)
 
   return user ? (
     <div>
       <CommonHeader />
       <Layout pageTitle={t('common', 'index.title')} {...user}>
-        {!showGrid && <Button onClick={() => setShowGrid(true)}>Edit</Button>}
-        <hr />
-        {showGrid && <Grid />}v{version}
+        v{version}
       </Layout>
     </div>
   ) : (
