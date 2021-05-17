9897c73a51322b9e4b9efeb26690130d9e0463cc
James Colton <james.c@pabau.com>
2021-05-17 07:26:13 +0100

    [skip ci] apollo generate

--- apollo.config.js
+++ apollo.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   client: {
     service: {
+      name: "hasura",
       url: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/v1/graphql' : 'https://api.new.pabau.com/v1/graphql',
+      headers: {
+        "X-Hasura-Admin-Secret": `${process.env.HASURA_GRAPHQL_ADMIN_SECRET}`
+      },
     },
   },
 }
