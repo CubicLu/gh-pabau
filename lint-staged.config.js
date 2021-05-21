7b6a1c483ac8c237e4b6ea9663cf8f126565d468
James Colton <james.c@pabau.com>
2021-05-21 10:31:21 +0000

    Merged in lint-staged-skip-merges (pull request #534)
    CI(lint-staged): skip during a git merge, skip eslint+prettier on same file
    
    * * skip whole script when there is a merge
    * skip prettier when the file i s handled by eslint
    
    * HF
    
    Approved-by: Toshe Boshkovski
    Approved-by: Stan N

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 // lint-staged.config.js
 const micromatch = require('micromatch')
 const prettier = require('prettier')
+const { execSync } = require('child_process')
 
 const prettierSupportedExtensions = prettier
   .getSupportInfo()
@@ -11,20 +12,36 @@ const prettierSupportedExtensions = prettier
 const addQuotes = (a) => `"${a}"`
 
 module.exports = (allStagedFiles) => {
-  const prettierFiles = micromatch(
-    allStagedFiles,
-    prettierSupportedExtensions.map((extension) => `**/*${extension}`)
-  )
   const ret = []
-  if (prettierFiles.length > 0)
-    ret.push(`prettier --write ${prettierFiles.map(addQuotes).join(' ')}`)
 
-  // const tscFiles = micromatch(allStagedFiles, ['**/*.{tsx,ts}'])
-  // if (tscFiles.length > 0) ret.push(`tsc --noEmit ${tscFiles.map(addQuotes).join(' ')}`)
+  let isInMerge = false
+  try {
+    execSync("git rev-parse -q --verify MERGE_HEAD", { stdio: "inherit" })
+    isInMerge = true
+  } catch {
+    console.log("exec sync failed, non-merge commit detected")
+  }
+  if (isInMerge) {
+    console.log("Skipping due to being inside a merge")
+    return []
+  }
 
+  // The master default is eslint. Which includes prettier.
   const eslintFiles = micromatch(allStagedFiles, ['**/*.{tsx,ts,jsx,js}'])
-  if (eslintFiles.length > 0)
+  if (eslintFiles.length > 0) {
     ret.push(`eslint --fix ${eslintFiles.map(addQuotes).join(' ')}`)
+  }
+
+  // Now for each unmatched file, see if prettier can handle it
+  for (const file of allStagedFiles.filter(e => eslintFiles.includes(e))) {
+    const prettierFiles = micromatch(
+      allStagedFiles,
+      prettierSupportedExtensions.map((extension) => `**/*${extension}`)
+    )
+    const ret = []
+    if (prettierFiles.length > 0)
+      ret.push(`prettier --write ${prettierFiles.map(addQuotes).join(' ')}`)
+  }
 
   return ret
 }
