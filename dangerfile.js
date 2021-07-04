3765ef8689c245c7754dc072982b7819694e5cc5
James Colton <james.c@pabau.com>
2021-07-04 01:25:07 +0100

    testing dangerjs

--- dangerfile.js
+++ dangerfile.js
@@ -1,7 +1,7 @@
 import { warn, message, danger } from 'danger'
 
-const modifiedMD = danger.git.modified_files.join('- ')
-message('Changed Files in this PR: \n - ' + modifiedMD)
+// const modifiedMD = danger.git.modified_files.join('- ')
+// message('Changed Files in this PR: \n - ' + modifiedMD)
 
 if (danger.bitbucket_cloud.pr.title.includes('WIP')) {
   warn('PR is considered WIP')
