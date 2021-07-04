55aca8421736eec95f05ad2a701e65bbafdb948e
James Colton <james.c@pabau.com>
2021-07-04 02:03:34 +0100

    try non-npx methods to run

--- apps/web/cicd/deploy.sh
+++ apps/web/cicd/deploy.sh
@@ -72,15 +72,8 @@ HEREDOC
 echo "${message_body}" >> /tmp/bot_message.txt
 
 echo "---- Deploying DB to Hasura Staging (api-v2-staging.pabau.com) ----"
-
-curl -L https://github.com/hasura/graphql-engine/raw/stable/cli/get.sh | INSTALL_PATH=$HOME/bin bash
+curl -LO https://github.com/hasura/graphql-engine/releases/download/v2.0.1/cli-hasura-linux-amd64
+chmod +x cli-hasura-linux-amd64
+sudo mv ./cli-hasura-linux-amd64 /usr/local/bin/hasura
 HASURA_GRAPHQL_ENDPOINT='https://api-v2-staging.pabau.com/' HASURA_GRAPHQL_ADMIN_SECRET="${HASURA_STAGING_GRAPHQL_ADMIN_SECRET}" "$HOME/bin/hasura" --project hasura migrate apply --database-name default || echo "SILENTLY FAILED"
-
-HASURA_GRAPHQL_ENDPOINT='https://api-v2-staging.pabau.com/' HASURA_GRAPHQL_ADMIN_SECRET="${HASURA_STAGING_GRAPHQL_ADMIN_SECRET}" npx -p hasura-cli hasura --project hasura migrate apply --database-name default || echo "SILENTLY FAILED"
-HASURA_GRAPHQL_ENDPOINT='https://api-v2-staging.pabau.com/' HASURA_GRAPHQL_ADMIN_SECRET="${HASURA_STAGING_GRAPHQL_ADMIN_SECRET}" npx -p hasura-cli hasura --project hasura metadata apply || echo "SILENTLY FAILED"
-
-HASURA_GRAPHQL_ENDPOINT='https://api-v2-staging.pabau.com/' HASURA_GRAPHQL_ADMIN_SECRET="${HASURA_STAGING_GRAPHQL_ADMIN_SECRET}" yarn hasura:cli migrate apply --database-name default || echo "SILENTLY FAILED"
-HASURA_GRAPHQL_ENDPOINT='https://api-v2-staging.pabau.com/' HASURA_GRAPHQL_ADMIN_SECRET="${HASURA_STAGING_GRAPHQL_ADMIN_SECRET}" yarn hasura:cli metadata apply || echo "SILENTLY FAILED"
-npm i -g hasura-cli
-HASURA_GRAPHQL_ENDPOINT='https://api-v2-staging.pabau.com/' HASURA_GRAPHQL_ADMIN_SECRET="${HASURA_STAGING_GRAPHQL_ADMIN_SECRET}" hasura --project hasura migrate apply --database-name default || echo "SILENTLY FAILED"
-HASURA_GRAPHQL_ENDPOINT='https://api-v2-staging.pabau.com/' HASURA_GRAPHQL_ADMIN_SECRET="${HASURA_STAGING_GRAPHQL_ADMIN_SECRET}" hasura --project hasura metadata apply|| echo "SILENTLY FAILED"
+HASURA_GRAPHQL_ENDPOINT='https://api-v2-staging.pabau.com/' HASURA_GRAPHQL_ADMIN_SECRET="${HASURA_STAGING_GRAPHQL_ADMIN_SECRET}" "$HOME/bin/hasura" --project hasura metadata apply || echo "SILENTLY FAILED"
