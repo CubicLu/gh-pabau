2d55b99f06873d167790223e7244798dbbc0107a
Dipak Savaliya <dipak241@gmail.com>
2021-07-05 17:30:22 +0530

    Fixed last notification remains in drawer

--- apps/web/components/Layout/Layout.tsx
+++ apps/web/components/Layout/Layout.tsx
@@ -96,6 +96,8 @@ const Layout: FC<LayoutProps> = ({
         }
       )
       setNotifications(todayNotification)
+    } else {
+      setNotifications([])
     }
   }, [notificationData?.notifications])
 
