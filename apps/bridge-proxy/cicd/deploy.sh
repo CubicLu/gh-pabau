868338ea2cdc4210b87424ce8b55acc5eb050c5c
James Colton <james.c@pabau.com>
2021-07-08 09:53:18 +0100

    CI: Hasura Migrations

--- apps/bridge-proxy/cicd/deploy.sh
+++ apps/bridge-proxy/cicd/deploy.sh
@@ -62,19 +62,4 @@ HEREDOC
   echo "${message_body}" >> /tmp/bot_message.txt
 fi
 
-# James testing deploying Hasura here, because we have access to docker command here
-echo "Deploying to Hasura database..."
-echo "Destination: ${HASURA_GRAPHQL_ENDPOINT}"
-mkdir -p dist
-cp -r hasura/ dist/
-#rm dist/hasura/metadata/actions.yaml
-cp -f hasura/remote_schemas.production.yaml dist/hasura/metadata/remote_schemas.yaml
-docker run --rm \
-  -v "${BITBUCKET_CLONE_DIR}/dist/hasura/:/hasura/:ro" \
-  -e HASURA_GRAPHQL_ENDPOINT="${HASURA_GRAPHQL_ENDPOINT}" \
-  -e HASURA_GRAPHQL_ADMIN_SECRET="${HASURA_STAGING_GRAPHQL_ADMIN_SECRET}" \
-  golang:buster \
-  bash -c "curl -LO https://github.com/hasura/graphql-engine/releases/download/v2.0.1/cli-hasura-linux-amd64 && chmod +x cli-hasura-linux-amd64 && mv ./cli-hasura-linux-amd64 /usr/local/bin/hasura && hasura version && hasura --project /hasura migrate apply --database-name default && hasura --project /hasura metadata apply" \
-  || echo "FAILED"
-
 echo "EOF"
