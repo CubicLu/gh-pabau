7c194a88e1e0a2cfc6cb745d8410afa618c6b9f0
Nenad Jovanovski <nenad@pabau.com>
2021-08-04 12:20:14 +0000

    [name].tsx edited online with Bitbucket

--- apps/web/pages/reports/[name].tsx
+++ apps/web/pages/reports/[name].tsx
@@ -1,4 +1,4 @@
-import { useGetReportByCodeQuery, useGetReportByIdQuery } from '@pabau/graphql'
+  import { useGetReportByCodeQuery, useGetReportByIdQuery } from '@pabau/graphql'
 import { useRouter } from 'next/router'
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
