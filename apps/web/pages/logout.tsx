7f496adbc440e1fc8eb6ffaf600caa6bd21b6cb9
Stan N <stan@pabau.com>
2021-08-06 08:41:51 +0000

    Merged in PABAU2-522 (pull request #749)
    PABAU2-522
    
    * Quick logout page for Pabau 1 purposes
    
    * Quick logout page for Pabau 1 purposes
    
    * Real quick
    
    * fresh pull
    
    * reverting mysql
    
    * Logout redirection
    
    * Logout redirection

--- apps/web/pages/logout.tsx
+++ apps/web/pages/logout.tsx
@@ -1,7 +1,10 @@
 import { Result } from 'antd'
+import { useRouter } from 'next/router'
 
 const Logout = () => {
   localStorage.clear()
+  const router = useRouter()
+  router.push('/login')
   return (
     <div>
       <Result title="Securely logging you out!" />,
