b41acb9799173174cbdb228faee0171d8b139c19
James Colton <james.c@pabau.com>
2021-08-07 19:19:25 +0100

    feat(ci): introducing esbuild [skip ci]

---
+++ tools/cli/esbuild.cjs
@@ -0,0 +1,16 @@
+const { nodeExternalsPlugin } = require('esbuild-node-externals');
+const esbuild = require('esbuild');
+
+console.log("Compiling apps/bridge-api/src/main.ts ...")
+
+esbuild.build({
+    entryPoints: ["apps/bridge-api/src/main.ts"],
+    bundle: true,
+    format: "cjs",
+    platform: "node",
+    outfile: "dist/asdf/main.cjs",
+    sourcemap: true,
+    plugins: [nodeExternalsPlugin()],
+  })
+  .then(() => console.log("Compiled."))
+  .catch(() => process.exit(1));
