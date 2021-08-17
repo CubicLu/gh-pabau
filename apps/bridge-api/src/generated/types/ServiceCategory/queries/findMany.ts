da98fbbc0cd049b5ab5900e4a6426215efccfbe2
Nenad Jovanovski <nenad@pabau.com>
2021-08-17 12:26:55 +0200

    asd

---
+++ apps/bridge-api/src/generated/types/ServiceCategory/queries/findMany.ts
@@ -0,0 +1,22 @@
+import { queryField, nonNull, list } from 'nexus'
+
+export const ServiceCategoryFindManyQuery = queryField(
+  'findManyServiceCategory',
+  {
+    type: nonNull(list(nonNull('ServiceCategory'))),
+    args: {
+      where: 'ServiceCategoryWhereInput',
+      orderBy: list('ServiceCategoryOrderByInput'),
+      cursor: 'ServiceCategoryWhereUniqueInput',
+      distinct: 'ServiceCategoryScalarFieldEnum',
+      skip: 'Int',
+      take: 'Int',
+    },
+    resolve(_parent, args, { prisma, select }) {
+      return prisma.serviceCategory.findMany({
+        ...args,
+        ...select,
+      })
+    },
+  },
+)
