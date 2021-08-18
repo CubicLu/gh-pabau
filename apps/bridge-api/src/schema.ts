a193297ddfa14a780293a4d79a5dfb203c676e36
James Colton <james.c@pabau.com>
2021-08-18 20:38:53 +0100

    wip

--- apps/bridge-api/src/schema.ts
+++ apps/bridge-api/src/schema.ts
@@ -12,7 +12,7 @@ const schema = makeSchema({
   shouldGenerateArtifacts: process.argv.includes('--nexus-typegen'),
   types,
   outputs: {
-    typegen: __dirname + '/generated/nexus.ts',
+    typegen: __dirname + '/generated/nexus.d.ts',
   },
   sourceTypes: {
     modules: [
