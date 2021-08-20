9966290b7f49c60170652abf21b4ba32da6e46d0
James Colton <james.c@pabau.com>
2021-08-20 02:07:57 +0100

    wip

---
+++ tools/cli/hasura-wait.ts
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch';
+import {config} from 'dotenv-flow'
+const retry = require("async-retry")
+
+config({path: 'hasura', default_node_env: 'development'})
+if (process.env.DEBUG) console.table({
+  cwd: process.cwd(),
+  HASURA_GRAPHQL_ENDPOINT: process.env.HASURA_GRAPHQL_ENDPOINT,
+  HASURA_GRAPHQL_ADMIN_SECRET: process.env.HASURA_GRAPHQL_ADMIN_SECRET,
+  NODE_ENV: process.env.NODE_ENV,
+})
+
+const HASURA_GRAPHQL_ENDPOINT = process.env.HASURA_GRAPHQL_ENDPOINT || "http://localhost:8080"
+
+const url = new URL("/v1/query", process.argv[2] || HASURA_GRAPHQL_ENDPOINT);
+
+retry(async (bail) => {
+  console.log(`fetching from ${url}`)
+  await fetch(url.toString(), {
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Hasura-Admin-Secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET
+    },
+    method: 'post',
+    body: JSON.stringify({})
+  })
+    .then(e => e.json())
+    .then(e => {
+      console.log("got json!")
+      if (!e || e.code !== "parse-failed") throw new Error("Error from remote " + JSON.stringify(e))
+    })
+}, { retries: 15 })
