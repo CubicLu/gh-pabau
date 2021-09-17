1a0b4af7cbeb01049932ea31f4623ed9d31d4800
Nivedita Singh <nivedita.ninja@gmail.com>
2021-09-17 18:54:07 +0530

    Added script for delete duplicate records

--- apps/bridge-api/src/prisma/migrations/20210914045512-PABAU2-840/up.sql
+++ apps/bridge-api/src/prisma/migrations/20210914045512-PABAU2-840/up.sql
@@ -1,4 +1,15 @@
 START TRANSACTION;
+DELETE FROM `cm_contacts_custom` 
+WHERE
+    `id` NOT IN (SELECT 
+        `MaxRecordID`
+    FROM
+        (SELECT 
+            MAX(`id`) AS `MaxRecordID`
+        FROM
+            `cm_contacts_custom`
+        GROUP BY `custom_field_id` , `occupier` , `contact_id`) AS c);
+        
 ALTER TABLE `cm_contacts_custom`
 ADD CONSTRAINT `unique_company_id_contact_id_custom_field_id` UNIQUE (`occupier`, `contact_id`, `custom_field_id`);
 COMMIT;
