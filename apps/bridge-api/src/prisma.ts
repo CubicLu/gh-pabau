22d0639e825d7adf5fa6b6fd2f6b78a6ced3667e
Suhani Shah <suhanishah228@gmail.com>
2021-09-30 14:14:01 +0530

    Resolved changes

--- apps/bridge-api/src/prisma.ts
+++ apps/bridge-api/src/prisma.ts
@@ -20,6 +20,7 @@ if (!PABAU1_MYSQL_USERNAME_PODS || !PABAU1_MYSQL_PASSWORD_PODS) {
 }
 
 function getPodDbUrl(urlOrHostname) {
+  return DATABASE_URL
   if (!urlOrHostname) return DATABASE_URL
 
   let url
