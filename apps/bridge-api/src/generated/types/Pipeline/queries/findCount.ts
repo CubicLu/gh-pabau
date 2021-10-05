862d62cc8cdf2f6a902daa666c890b4c5236ffd3
branko tasevski <branko@pabau.com>
2021-10-05 09:26:56 +0000

    Merged in PABAU2-1022 (pull request #938)
    adding new column
    
    * update
    
    * auto generated
    
    * adding queru getStages
    
    * Merged master into PABAU2-1022
    * adding queru getStages
    
    * adding queru getStages
    
    * bumping version
    
    * Merge branch 'master' of https://bitbucket.org/pabau/monorepo into PABAU2-1022
    
    * update queru getStages
    
    * Merged master into PABAU2-1022

---
+++ apps/bridge-api/src/generated/types/Pipeline/queries/findCount.ts
@@ -0,0 +1,16 @@
+import { queryField, nonNull, list } from 'nexus'
+
+export const PipelineFindCountQuery = queryField('findManyPipelineCount', {
+  type: nonNull('Int'),
+  args: {
+    where: 'PipelineWhereInput',
+    orderBy: list('PipelineOrderByWithRelationInput'),
+    cursor: 'PipelineWhereUniqueInput',
+    distinct: 'PipelineScalarFieldEnum',
+    skip: 'Int',
+    take: 'Int',
+  },
+  resolve(_parent, args, { prisma }) {
+    return prisma.pipeline.count(args as any)
+  },
+})
