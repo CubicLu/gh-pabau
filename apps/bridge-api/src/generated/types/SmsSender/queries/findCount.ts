403e849452c689c25a57b5060d89466428910fef
Kiko <kiko@pabau.com>
2021-10-12 10:58:39 +0200

    PABAU2-617

---
+++ apps/bridge-api/src/generated/types/SmsSender/queries/findCount.ts
@@ -0,0 +1,16 @@
+import { queryField, nonNull, list } from 'nexus'
+
+export const SmsSenderFindCountQuery = queryField('findManySmsSenderCount', {
+  type: nonNull('Int'),
+  args: {
+    where: 'SmsSenderWhereInput',
+    orderBy: list('SmsSenderOrderByWithRelationInput'),
+    cursor: 'SmsSenderWhereUniqueInput',
+    distinct: 'SmsSenderScalarFieldEnum',
+    skip: 'Int',
+    take: 'Int',
+  },
+  resolve(_parent, args, { prisma }) {
+    return prisma.smsSender.count(args as any)
+  },
+})
---
+++ apps/bridge-api/src/generated/types/SmsSender/queries/findFirst.ts
@@ -0,0 +1,19 @@
+import { queryField, list } from 'nexus'
+
+export const SmsSenderFindFirstQuery = queryField('findFirstSmsSender', {
+  type: 'SmsSender',
+  args: {
+    where: 'SmsSenderWhereInput',
+    orderBy: list('SmsSenderOrderByWithRelationInput'),
+    cursor: 'SmsSenderWhereUniqueInput',
+    distinct: 'SmsSenderScalarFieldEnum',
+    skip: 'Int',
+    take: 'Int',
+  },
+  resolve(_parent, args, { prisma, select }) {
+    return prisma.smsSender.findFirst({
+      ...args,
+      ...select,
+    })
+  },
+})
