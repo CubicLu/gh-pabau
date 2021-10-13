570d75da3f02b36aae79d628e15cfaeaef79a1c6
Branko Tasevski <branko@pabau.com>
2021-10-13 09:36:39 +0200

    PABAU2-591

--- apps/bridge-api/src/resolvers/types/Booking.ts
+++ apps/bridge-api/src/resolvers/types/Booking.ts
@@ -1,5 +1,4 @@
-import { extendType, list, nonNull, intArg } from 'nexus'
-import { Booking, User } from '../../generated/types'
+import { extendType, list, nonNull } from 'nexus'
 import { Context } from '../../context'
 
 export const BookingExtended = extendType({
@@ -16,25 +15,23 @@ export const BookingExtended = extendType({
         for (const item of usersIds?.split(',')) {
           ids.push(Number.parseInt(item))
         }
-        const users = await ctx.prisma.user.findMany({
+        return await ctx.prisma.user.findMany({
           where: {
             id: { in: ids },
           },
         })
-        return users
       },
     })
     t.field('BookedBy', {
       type: nonNull(list('User')),
       async resolve(parent: any, args, ctx: Context) {
-        const userCreated = await ctx.prisma.user.findMany({
+        return await ctx.prisma.user.findMany({
           where: {
             id: {
               equals: parent.created_by_uid,
             },
           },
         })
-        return userCreated
       },
     })
   },
