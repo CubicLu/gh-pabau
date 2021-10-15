ced170506e9e6f973c1fe6e70cb8566f35402ae2
Siddharth Singh <sidsingh760@gmail.com>
2021-10-15 16:05:40 +0530

    Minior changes

--- apps/web/components/Email/loginDynamic.tsx
+++ apps/web/components/Email/loginDynamic.tsx
@@ -6,37 +6,6 @@ export interface P {
 export const Login: FC<P> = ({ handleGoogleLogin }) => {
   const userSignIn = false
 
-  // const handleClick = () => {
-  //   const oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth'
-  //
-  //   const form = document.createElement('form')
-  //   form.setAttribute('method', 'GET')
-  //   form.setAttribute('action', oauth2Endpoint)
-  //
-  //   const params = {
-  //     client_id:
-  //       '1006619281478-0ggfmclia2856fnes3640qn7rhq1f2u9.apps.googleusercontent.com',
-  //     redirect_uri: `${window.location.origin}/setup/senders`,
-  //     access_type: 'offline',
-  //     response_type: 'code',
-  //     scope: 'https://mail.google.com/',
-  //     include_granted_scopes: 'true',
-  //     state: 'state_parameter_passthrough_value',
-  //   }
-  //
-  //   for (const p in params) {
-  //     const input = document.createElement('input')
-  //     input.setAttribute('type', 'hidden')
-  //     input.setAttribute('name', p)
-  //     input.setAttribute('value', params[p])
-  //     form.append(input)
-  //   }
-  //
-  //   document.body.append(form)
-  //   form.submit()
-  //   return handleGoogleLogin()
-  // }
-
   useEffect(() => {
     const oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth'
 
