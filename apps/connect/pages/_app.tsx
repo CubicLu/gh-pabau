320a8d82a444b5d4f9b3330a0ced108e6f9c19e5
Nenad Jovanovski <nenad@pabau.com>
2021-10-18 23:37:19 +0200

    updates

--- apps/connect/pages/_app.tsx
+++ apps/connect/pages/_app.tsx
@@ -2,11 +2,75 @@ import React from 'react'
 import { AppProps } from 'next/app'
 import TranslationWrapper from '../components/TranslationWrapper'
 import SettingsContextWrapper from '../components/SettingsContextWrapper'
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  split,
+  ApolloLink,
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { WebSocketLink } from '@apollo/client/link/ws'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { OperationDefinitionNode } from 'graphql'
 require('../../../libs/ui/src/styles/antd.less')
 
+const GRAPHQL_WS_ENDPOINT =
+  process.env.NEXT_PUBLIC_WS_ENDPOINT || 'wss://api.new.pabau.com/v1/graphql'
+const GRAPHQL_HTTP_ENDPOINT =
+  process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ||
+  'https://api.new.pabau.com/v1/graphql'
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('connect_token')
+  let token2 = null
+  if (token) token2 = JSON.parse(token)
+  return {
+    headers: token2
+      ? {
+          ...headers,
+          authorization: `Bearer ${token2}`,
+        }
+      : headers,
+  }
+})
+const wsLink = process.browser
+  ? new WebSocketLink({
+      uri: GRAPHQL_WS_ENDPOINT,
+      options: {
+        reconnect: true,
+        connectionParams: {
+          authToken: '',
+        },
+      },
+    })
+  : null
+const httpLink = new HttpLink({
+  fetch,
+  uri: GRAPHQL_HTTP_ENDPOINT,
+})
+
+const terminatingLink = wsLink
+  ? split(
+      ({ query }) => {
+        const { kind, operation } = getMainDefinition(
+          query
+        ) as OperationDefinitionNode
+        return (
+          kind === 'OperationDefinition' &&
+          operation === 'subscription' &&
+          process.browser
+        )
+      },
+      wsLink,
+      httpLink
+    )
+  : httpLink
+
 const client = new ApolloClient({
-  uri: 'http://localhost:8080/v1/graphql',
+  ssrMode: false,
+  link: ApolloLink.from([authLink, terminatingLink]),
   cache: new InMemoryCache({
     addTypename: false,
   }),
