9c41a3bccb8b12480bdc463f97984d6b5fde224f
Nenad Jovanovski <nenad@pabau.com>
2021-10-21 18:17:52 +0200

    updates

--- libs/ui/src/lib/gp-search/Map.tsx
+++ libs/ui/src/lib/gp-search/Map.tsx
@@ -1,14 +1,53 @@
-import { render } from '@testing-library/react'
-import Index from '../pages/index'
-import { MockedProvider } from '@apollo/client/testing'
+import React, { FC, ReactNode } from 'react'
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from 'react-google-maps'
+import { compose, withProps } from 'recompose'
+import { LocationDetails } from './GpSearch'
 
-describe('Index', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(
-      <MockedProvider>
-        <p>test</p>
-      </MockedProvider>
+interface MapProps {
+  apiKey: string
+  locationDetail: LocationDetails
+  isMarkerShown?: boolean
+  setLocationDetail: ReactNode
+}
+
+const Map: FC<MapProps> = compose(
+  withProps((props) => ({
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${props.apiKey}&v=3.exp&libraries=geometry,drawing,places`,
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `100%`, width: '100%' }} />,
+    mapElement: <div style={{ height: `100%`, borderRadius: 4 }} />,
+  })),
+  withScriptjs,
+  withGoogleMap
+)(
+  (props: {
+    apiKey
+    locationDetail
+    onSearchBoxMounted
+    bounds
+    onPlacesChanged
+    isMarkerShown
+    setLocationDetail
+  }) => {
+    return (
+      <GoogleMap
+        defaultOptions={{
+          disableDefaultUI: true,
+        }}
+        defaultZoom={14}
+        center={props.locationDetail.position}
+      >
+        {props.isMarkerShown && (
+          <Marker position={props.locationDetail.position} />
+        )}
+      </GoogleMap>
     )
-    expect(baseElement).toBeTruthy()
-  })
-})
+  }
+)
+
+export default Map
