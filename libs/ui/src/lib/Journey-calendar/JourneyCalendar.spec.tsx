ee44f5bd5310e9e968952063cc7ec39e02a9df70
Vimal <vimalbhalani23@gmail.com>
2021-10-21 20:53:01 +0530

    PABAU2-1558: made swipeable and scroling animation

--- libs/ui/src/lib/Journey-calendar/JourneyCalendar.spec.tsx
+++ libs/ui/src/lib/Journey-calendar/JourneyCalendar.spec.tsx
@@ -5,7 +5,14 @@ import JourneyCalendar from './JourneyCalendar'
 
 describe('JourneyCalendar', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<JourneyCalendar />)
+    const { baseElement } = render(
+      <JourneyCalendar
+        setActiveDate={(val) => {
+          console.log(val)
+        }}
+        activeDate="Oct 21 2021"
+      />
+    )
     expect(baseElement).toBeTruthy()
   })
 })
