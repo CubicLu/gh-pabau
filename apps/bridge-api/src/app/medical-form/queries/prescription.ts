c7ddcfff84faf5e4cde4390eb8bbaff1c1c64bb6
kostovaljubica <kostova@pabau.com>
2021-10-28 08:21:51 +0200

    refactor

--- apps/bridge-api/src/app/medical-form/queries/prescription.ts
+++ apps/bridge-api/src/app/medical-form/queries/prescription.ts
@@ -2,32 +2,32 @@ import { extendType, list, nonNull } from 'nexus'
 import { Context } from '../../../context'
 
 import { Base64 } from 'js-base64'
+import { MedicalForm } from '@prisma/client'
 
 export const MedicalFormExtended = extendType({
   type: 'MedicalForm',
   definition(t) {
     t.field('Drugs', {
       type: nonNull(list('CmDrug')),
-      async resolve(parent: any, args, ctx: Context) {
-        if (parent.data) {
-          const form = Base64.decode(parent.data)
-          const form2 = JSON.parse(form)
-          const drug_ids = form2.form_structure
-            .map((item) => {
-              if (item.cssClass === 'cl_drugs') return item.values
-            })
-            .join(',')
-          const drug_ids_arr = drug_ids.split(',').map(function (i) {
-            return Number.parseInt(i, 10)
-          })
-          return await ctx.prisma.cmDrug.findMany({
-            where: {
-              id: { in: drug_ids_arr },
-            },
-          })
-        }
+      async resolve(parent: MedicalForm, args, ctx: Context) {
+        if (!parent.data) return []
 
-        return []
+        const form = JSON.parse(Base64.decode(parent.data))
+        const drug_ids = form.form_structure
+          .map((item) => {
+            if (item.cssClass === 'cl_drugs') return item.values
+            return ''
+          })
+          .join(',')
+
+        const drug_ids_arr = drug_ids.split(',').map(function (i) {
+          return Number.parseInt(i, 10)
+        })
+        return await ctx.prisma.cmDrug.findMany({
+          where: {
+            id: { in: drug_ids_arr },
+          },
+        })
       },
     })
   },
