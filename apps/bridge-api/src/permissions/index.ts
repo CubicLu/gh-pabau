101436e841d7acb2bc6d5426a61ec3eadd8e4dbd
Branko Tasevski <branko@pabau.com>
2021-11-16 12:54:46 +0100

    Add pathway permission

--- apps/bridge-api/src/permissions/index.ts
+++ apps/bridge-api/src/permissions/index.ts
@@ -128,6 +128,7 @@ export const permissions = shield(
       updateOneCmContact: rules.authentication.isAuthenticated,
       // Pathway
       upsertOnePathwayStepsTaken: rules.interceptors.injectContact,
+      createOnePathwaysTaken: allow,
       // Default fallback
       '*': and(
         rules.authentication.isAuthenticated,
