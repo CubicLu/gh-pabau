a9c73ec00c13d58f0ef35759e34cb0bc14807872
Stan <stan@pabau.com>
2021-11-17 13:28:21 +0100

    Nextjs12

--- apps/connect/specs/index.spec.tsx
+++ apps/connect/specs/index.spec.tsx
@@ -1,28 +1,29 @@
-// import { render } from '@testing-library/react'
-// import Index from '../pages/index'
-// import { MockedProvider } from '@apollo/client/testing'
+import { render } from '@testing-library/react'
+import Index from '../pages/index'
+import { MockedProvider } from '@apollo/client/testing'
+import * as React from 'react'
 
-// //manual mock to handle matchMedia not found function ref: https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
-// Object.defineProperty(window, 'matchMedia', {
-//   writable: true,
-//   value: jest.fn().mockImplementation((query) => ({
-//     matches: false,
-//     media: query,
-//     onchange: null,
-//     addListener: jest.fn(), // deprecated
-//     removeListener: jest.fn(), // deprecated
-//     addEventListener: jest.fn(),
-//     removeEventListener: jest.fn(),
-//     dispatchEvent: jest.fn(),
-//   })),
-// })
+//manual mock to handle matchMedia not found function ref: https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+})
 
-// it('should render successfully', () => {
-//   const { baseElement } = render(
-//     <MockedProvider>
-//       {/*<Index />*/}
-//       <h1>temporarily disabled</h1>
-//     </MockedProvider>
-//   )
-//   expect(baseElement).toBeTruthy()
-// })
+it('should render successfully', () => {
+  const { baseElement } = render(
+    <MockedProvider>
+      {/*<Index />*/}
+      <h1>temporarily disabled</h1>
+    </MockedProvider>
+  )
+  expect(baseElement).toBeTruthy()
+})
