282a21d773224b27d7206a0034b4dbfa39b04582
James Colton <james.c@pabau.com>
2021-12-03 10:58:18 +0000

    Merged in ci_changes (pull request #1201)
    CI changes
    
    * fix(ci): nexus generate before graphql-codegen can create a hash.
    
    * fix(ci): remove useless files
    
    * fix(ci): remove useless root
    
    * fix(ci): reinstate the git codegeneration checks
    
    
    Approved-by: Alek Angelov
    Approved-by: Stan N

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,6 @@ module.exports = {
     '<rootDir>/apps/backend',
     '<rootDir>/apps/mgmt',
     '<rootDir>/apps/connect',
-    '<rootDir>/apps/mobile',
     '<rootDir>/apps/bridge-api',
     '<rootDir>/libs/i18n',
     '<rootDir>/apps/storage',
